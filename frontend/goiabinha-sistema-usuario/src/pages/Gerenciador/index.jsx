086521e0270ed90c780d72b8e470053e24e52a0c
Merces-dev <giovanitjs013@gmail.com>
2021-09-15 17:21:07 -0300

    Adição das funções ListarUsuarios e ExcluirUsuarios

--- frontend/goiabinha-sistema-usuario/src/pages/Gerenciador/index.jsx
+++ frontend/goiabinha-sistema-usuario/src/pages/Gerenciador/index.jsx
@@ -1,26 +1,100 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react'
 import Header from './../../components/header'
+
+//Importando botões com Ícones
 import {FaTrashAlt, FaPencilAlt} from 'react-icons/fa'
 
+
+//Importando a Url que será utilizada para realizar o fetch (Ligação com a API)
+import { url } from '../../utils/constants'
+
+//Importando CSS
 import './index.css'
+
 const Gerenciador = () => {
-    const AdicionarUsuario = () =>{
 
+    // Declarando Variáveis
+    const [idUsuario, setIdUsuario] = useState('');
+    const [nome, setNome] = useState('');
+    const [dataNascimento, setDataNascimento] = useState('');
+    const [sexo, setSexo] = useState('');
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(() =>{
+        listarUsuarios()
+    },[]);
+
+    // Método que adiciona um novo usuário
+    const adicionarUsuario = () =>{
+        fetch(`${url}/Usuarios`, {
+            method: 'POST',
+        })
+        .then(response => response.json())
+        .then(dados => {
+            alert('Usuario Alterado!');
+            listarUsuarios();
+        })
+        .catch(err => console.error(err));
     }
 
-    const AtualizarUsuario = () =>{
-
+    // Método que atualiza as informações de um usuario de acordo com seu Id
+    const atualizarUsuario = (event) =>{
+        fetch(`${url}/Usuarios/${event.target.value}`, {
+            method: 'PUT',
+            //TO-DO 
+            // Adicionar authorization com bearer token
+        })
+        .then(response => response.json())
+        .then(dados => {
+            alert('Usuario Alterado!');
+            listarUsuarios();
+        })
+        .catch(err => console.error(err));
     }
 
-    const ExcluirUsuario = () =>{
-
-    }
-
-    const ListarUsuario = () =>{
-
-    }
+    // Método que exclui um usuario e suas informações de acordo com seu Id
+    const excluirUsuario = (event) =>{
+        if(event.target.value != undefined){
+            fetch(`${url}/Usuarios/${event.target.value}`, {
+                method: 'DELETE',
+                //TO-DO 
+                // Adicionar authorization com bearer token
+            })
+            .then(response => response.json())
+            .then(dados => {
+                console.log(event.target.value)
     
-    const ListarUsuarios = () =>{
+                alert('Usuario removido!');
+                listarUsuarios();
+            })
+            .catch(err => console.error(err));
+        }else{
+            alert('Usuario não encontrado, tente novamente!');  
+        }
+  
+    }
+
+    // Método que lista um usuario e suas informações de acordo com seu Id
+    const listarUsuario = (id) =>{
+        fetch(`${url}/Usuarios/${id}`)
+        .then(response => response.json())
+        .then(dados => {
+            setUsuarios(dados);
+            console.log(usuarios)
+        })
+        .catch(err => console.error(err));
+    }
+
+    // Método que lista todos os usuários e suas informações
+    const listarUsuarios = () =>{
+        fetch(`${url}/Usuarios`)
+        .then(response => response.json())
+        .then(dados => {
+            setUsuarios(dados);
+            console.log(usuarios)
+        })
+        .catch(err => console.error(err));
 
     }
 
@@ -46,23 +120,30 @@ const Gerenciador = () => {
                                         <td>Nome</td>
                                         <td>Data de Nascimento</td>
                                         <td>Sexo</td>
-
-                                        <td></td>
+                                        <td>Ações</td>
                                     </tr>
                                 </thead>
 
                                 <tbody>
-                                    <tr>
-                                        <th>1</th>
-                                        <th>Giovani Merces</th>
-                                        <th>01/04/2003</th>
-                                        <th>Masculino</th>
-                                        <th className='colunaBotoes'>
-                                            <button style={{backgroundColor:'#0abab5'}}><FaPencilAlt className='iconBotoes'/></button>
-                                            <button style={{backgroundColor:'#ff3333'}}><FaTrashAlt className='iconBotoes'/></button>
-                                        </th>
+                                    {
+                                    // Função que faz a listagem de todos os usuários em forma de lista
+                                    usuarios.map((item, index) => {
+                                            return(
+                                            <tr key={index}>
+                                                <th>{item.id}</th>
+                                                <th>{item.nome}</th>
+                                                <th>{item.dataNascimento}</th>
+                                                <th>{item.sexo}</th>
+                                                <th className='colunaBotoes'>
+                                                    <button style={{backgroundColor:'#0abab5'}} value={item.id} onClick={event => atualizarUsuario(event)} ><FaPencilAlt className='iconBotoes'/></button>
+                                                    <button style={{backgroundColor:'#ff3333'}} value={item.id} onClick={event => excluirUsuario(event)}><FaTrashAlt className='iconBotoes'/></button>
+                                               
+                                                </th>
 
-                                    </tr>
+                                            </tr>
+                                            )
+                                        })
+                                    }
                                 </tbody>
 
                             </table>
---
+++ frontend/goiabinha-sistema-usuario/src/utils/constants.jsx
@@ -0,0 +1,1 @@
+export const url = 'https://6142308e4d16670017ba2c64.mockapi.io/api/v1';
